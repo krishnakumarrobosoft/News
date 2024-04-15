ad96413ab1cb46b7d5b9ee1069d65ed6bd9bfbf8
Krishna Kumar K <krishnakumar.k@robosoftin.com>
2024-04-15 18:06:57 +0530

    - Modified navigation keys

--- common/navigation/global/presentation/models/src/main/java/com/news/models/keys/GlobalNavigationKeys.kt
+++ common/navigation/global/presentation/models/src/main/java/com/news/models/keys/GlobalNavigationKeys.kt
@@ -1,7 +1,7 @@
 package com.news.models.keys
 
 
-internal const val NONE = "NONE"
-internal const val HOME = "HOME"
-internal const val ARTICLE_DETAIL = "ARTICLE_DETAIL"
-internal const val BACK = "BACK"
+internal const val NONE = "none"
+internal const val HOME = "home"
+internal const val ARTICLE_DETAIL = "articleDetail"
+internal const val BACK = "back"
